64e3d185b5511ae8ac463dce0aa635528b60615c
H. Burdy <58144965+hburdy@users.noreply.github.com>
2021-03-01 16:11:59 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Sliding Sign In Form
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
+
+signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
+
+// End Of Sliding In Form
